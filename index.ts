a334f0ee676fa813ed9fe6bf059a376023f462bb
Andy Lee <a.lee.intl@gmail.com>
2023-04-16 02:39:15 +0200

    export_types

--- index.ts
+++ index.ts
@@ -1,4 +1,26 @@
 import useAudiotracks from "./src/useAudiotracks"
 import AudiotrackManager from "./src/AudiotrackManager"
+import type {
+  SubtitlesJSON,
+  Subtitle,
+  Audio,
+  AudioOptions,
+  CaptionState,
+  Track,
+  PlayRequest,
+  AudioManagerState,
+  AudiotrackManagerState,
+} from "./src/types"
 
 export { useAudiotracks, AudiotrackManager }
+export type {
+  SubtitlesJSON,
+  Subtitle,
+  Audio,
+  AudioOptions,
+  CaptionState,
+  Track,
+  PlayRequest,
+  AudioManagerState,
+  AudiotrackManagerState,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 import useAudiotracks from "./useAudiotracks"
 import AudiotrackManager from "./AudiotrackManager"
+import type {
+  SubtitlesJSON,
+  Subtitle,
+  Audio,
+  AudioOptions,
+  CaptionState,
+  Track,
+  PlayRequest,
+  AudioManagerState,
+  AudiotrackManagerState,
+} from "./types"
 
 export { useAudiotracks, AudiotrackManager }
+export type {
+  SubtitlesJSON,
+  Subtitle,
+  Audio,
+  AudioOptions,
+  CaptionState,
+  Track,
+  PlayRequest,
+  AudioManagerState,
+  AudiotrackManagerState,
+}
