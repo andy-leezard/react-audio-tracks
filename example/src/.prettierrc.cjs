96cd92b332de896b2234f6319405f0be1b49d739
Andy Lee <a.lee.intl@gmail.com>
2023-11-07 13:07:37 +0100

    dev/use localized prettier settings

---
+++ example/src/.prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  arrowParens: "always",
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'auto',
+}
